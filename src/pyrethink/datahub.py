052b96a5fa120fa6022eb37e3a5e85566059548e
Julien Lerat <julien.lerat@csiro.au>
2025-10-21 12:58:08 +1100

    fix: fixed datahub

--- src/pyrethink/datahub.py
+++ src/pyrethink/datahub.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import re
 import json
-import numpy as np
-import pandas as pd
+
+from hydrodiy.io import csv
 
 FHERE = Path(__file__).resolve().parent
 FROOT = FHERE.parent.parent
@@ -12,6 +12,7 @@ ENV = "LOCAL"
 with (FHERE / "config.json").open("r") as fo:
     CONFIG = json.load(fo)[ENV]
 
+
 def replace_root(path):
     root_label = "package_root_folder"
     if path.startswith(root_label):
@@ -19,7 +20,19 @@ def replace_root(path):
     else:
         return path
 
+
 DATA_FOLDER = replace_root(CONFIG["data_folder"])
 
+DATA_VERSION = CONFIG["data_version"]
 
 
+def get_stations():
+    fs = DATA_FOLDER / f"AMS_stations_v{DATA_VERSION}.csv"
+    df, _ = csv.read_csv(fs, index_col="STATIONID")
+    return df
+
+
+def get_truepeaks():
+    ft = DATA_FOLDER / f"peak_streamflow_concatenated_v{DATA_VERSION}.csv"
+    df, _ = csv.read_csv(ft, index_col="DAY", parse_dates=True)
+    return df
--- tests/test_datahub.py
+++ tests/test_datahub.py
@@ -1,7 +1,13 @@
 import pytest
 
-from mypackage import datahub
+from pyrethink import datahub
 
 def test_data_folder():
     fd = datahub.DATA_FOLDER
     assert fd.exists()
+
+def test_get_stations():
+    df = datahub.get_stations()
+
+def test_truepeaks():
+    df = datahub.get_truepeaks()
