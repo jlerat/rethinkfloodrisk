9b049e8e3dd1cb407d4acfb1b98efa570ab4ff53
Julien Lerat <julien.lerat@csiro.au>
2025-11-05 14:48:11 +1100

    fix: fixed submit job script

--- scripts/fit/job_submit.sh
+++ scripts/fit/job_submit.sh
@@ -20,31 +20,61 @@ if [ ! -f "$jobscript" ]; then
     exit 1
 fi
 
-job1=copulafit
-job2=postprocess
+JOBNAMES=(
+    copulafit
+    postprocess
+)   
 
-echo
-echo -----------------------
-echo JOBNAME = $jobname
-echo
-echo ARRAYS = $ARRAYS
-echo -----------------------
-echo
+jobidprev=-999
 
-# Create log folder
-FLOG1=$FROOT/logs/$job1
-mkdir -p $FLOG1
+for jobname in "${JOBNAMES[@]}"; do
+    echo
+    echo ----------------------------------
+    echo JOBNAME = $jobname
 
-FLOG2=$FROOT/logs/$job2
-mkdir -p $FLOG2
+    if [ $jobname = "copulafit" ]
+    then
+        ncpus=10
+    else
+        ncpus=1
+    fi
+    echo .. ncpus = $ncpus
 
-# Run job
-if [ $ARRAYS = "none" ]
-then
-    job1id=$(sbatch -J $job1 --parsable --export=ALL $jobscript)
-    sbatch -J $job2 --dependency=afterany:${job1id} --export=ALL $jobscript
-else
-    job1id=$(sbatch -J $job1 --array=$ARRAYS --parsable --export=ALL $jobscript)
-    sbatch -J $job2 --array=$ARRAYS --dependency=afterany:${job1id} --export=ALL $jobscript
-fi
+    # Create log folder
+    FLOG=$FROOT/logs/$jobname
+    mkdir -p $FLOG
+    echo .. created log folder logs/$jobname
+
+    # Run job
+    if [ $ARRAYS = "none" ]
+    then
+        if [ $jobidprev = -999 ]
+        then
+            echo ... submitting job with no array and no dependency
+            jobid=$(sbatch -J $jobname --cpus-per-task=$ncpus \
+                --parsable --export=ALL $jobscript)
+        else
+            echo ... submitting job with no array and dependency on $jobidprev
+            sbatch -J $jobname --cpus-per-task=$ncpus \
+                --dependency=afterany:${jobidprev} --export=ALL $jobscript
+        fi    
+    else
+        if [ $jobidprev = -999 ]
+        then
+            echo ... submitting job with array $ARRAYS and no dependency
+            jobid=$(sbatch -J $jobname --cpus-per-task=$ncpus \
+                --array=$ARRAYS --parsable --export=ALL $jobscript)
+        else    
+            echo ... submitting job with array $ARRAYS and dependency on $jobidprev
+            sbatch -J $jobname --cpus-per-task=$ncpus \
+                --array=$ARRAYS --dependency=afterany:${jobidprev} --export=ALL $jobscript
+        fi    
+    fi
+
+    # Iterate jobid
+    jobidprev=$jobid
+
+    echo ... All done. Moving to next job.
+done    
+
 
