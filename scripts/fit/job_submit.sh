12c2bd18cdefeb538ad1dc625b10be13bc06196f
Julien Lerat <julien.lerat@csiro.au>
2025-11-12 11:26:35 +1100

    fix: fixed job script [ci skip]

--- scripts/fit/job_submit.sh
+++ scripts/fit/job_submit.sh
@@ -13,94 +13,106 @@ else
 fi
 
 # Job config
-jobscript=$FROOT/scripts/fit/job_script.job
+JOBSCRIPT=$FROOT/scripts/fit/job_script.job
 
-if [ ! -f "$jobscript" ]; then
-    echo "ERROR - script $jobscript does not exists"
+if [ ! -f "$JOBSCRIPT" ]; then
+    echo "ERROR - script $JOBSCRIPT does not exists"
     exit 1
 fi
 
+# Mimick a 2d array using 1d bash arrays
 declare -A JOBCONFIGS
 
 # Copula fitting job
-JOBCONFIGS[0,0] = "copulafit"  # Job name
-JOBCONFIGS[0,1] = "10"         # number of cpus
-JOBCONFIGS[0,2] = "0-5"        # arrays
-JOBCONFIGS[0,3] = "1"          # use user supplied array if any
-JOBCONFIGS[0,4] = "X"          # job dependency
-JOBCONFIGS[0,5] = "X"          # job id
+JOBCONFIGS["0,0"]="copulafit"  # Job name
+JOBCONFIGS["0,1"]="10"         # number of cpus
+JOBCONFIGS["0,2"]="0-5"        # arrays
+JOBCONFIGS["0,3"]="1"          # use user supplied array if any
+JOBCONFIGS["0,4"]="X"          # job dependency
+JOBCONFIGS["0,5"]="X"          # job id
 
 # General postprocessing job
-JOBCONFIGS[1,0] = "postprocess"  # Job name
-JOBCONFIGS[1,1] = "1"            # number of cpus
-JOBCONFIGS[1,2] = "0-5"          # arrays
-JOBCONFIGS[1,3] = "1"            # use user supplied array if any
-JOBCONFIGS[1,4] = "0"            # parent job number
-JOBCONFIGS[1,5] = "X"            # job id
+JOBCONFIGS["1,0"]="postprocess"  # Job name
+JOBCONFIGS["1,1"]="1"            # number of cpus
+JOBCONFIGS["1,2"]="0-5"          # arrays
+JOBCONFIGS["1,3"]="1"            # use user supplied array if any
+JOBCONFIGS["1,4"]="0"            # parent job number
+JOBCONFIGS["1,5"]="X"            # job id
 
 # MVN postprocessing job
-JOBCONFIGS[2,0] = "mvnprocess"   # Job name
-JOBCONFIGS[2,1] = "1"            # number of cpus
-JOBCONFIGS[2,2] = "0-799"        # arrays
-JOBCONFIGS[2,3] = "0"            # use user supplied array if any
-JOBCONFIGS[2,4] = "0"            # parent job number
-JOBCONFIGS[2,5] = "X"            # job id
+JOBCONFIGS["2,0"]="mvnprocess"   # Job name
+JOBCONFIGS["2,1"]="1"            # number of cpus
+JOBCONFIGS["2,2"]="0-799"        # arrays
+JOBCONFIGS["2,3"]="0"            # use user supplied array if any
+JOBCONFIGS["2,4"]="0"            # parent job number
+JOBCONFIGS["2,5"]="X"            # job id
 
-NJOBS=3
+LENGTH=${#JOBCONFIGS[@]}
+NJOBS=$(($LENGTH / 6))    
+
+echo
+echo "**************************************************"
+echo Processing $NJOBS JOBS
+echo User supplied arrays : $USER_SUPPLIED_ARRAYS
+echo
+echo FROOT = $FROOT
+echo "**************************************************"
+echo
 
 for ((ijob = 0; ijob < $NJOBS; ijob ++)); do
     # Retrieve config
-    jobname=$JOBCONFIGS[$ijob,0]
-    ncpus=$JOBCONFIGS[$ijob,1]
-    arrays=$JOBCONFIGS[$ijob,2]
-    overwrite_arrays=$JOBCONFIGS[$ijob,3]
-    parent_job=$JOBCONFIGS[$ijob,4]
+    jobname=${JOBCONFIGS[$ijob,0]}
+    ncpus=${JOBCONFIGS[$ijob,1]}
+    arrays=${JOBCONFIGS[$ijob,2]}
+    overwrite_arrays=${JOBCONFIGS[$ijob,3]}
+    parent_job=${JOBCONFIGS[$ijob,4]}
 
     # Overwrite array with user supplied argument
-    if [ $overwrite_arrays eq "1"] && [ $USER_SUPPLIED_ARRAYS -ne "X"]
+    if [[ $overwrite_arrays = "1" && $USER_SUPPLIED_ARRAYS != "X" ]]
     then
         arrays=$USER_SUPPLIED_ARRAYS        
     fi
     
     # Get parent job id
-    if [ $parent_job -neq "X" ] 
+    if [ $parent_job != "X" ] 
     then
         parent_ijob=$((parent_job))
-        parent_jobid=$JOBCONFIGS[$parent_ijob,5]
+        parent_jobid=${JOBCONFIGS[$parent_ijob,5]}
     else    
         parent_jobid="X"
     fi
 
     echo
-    echo ----------------------------------
+    echo ----------------------------------------------------------
     echo JOB \#$ijob
-    echo   NAME     : $jobname
-    echo   NCPUS    : $ncpus
-    echo   ARRAYS   : $arrays
-    echo   PJOBID   : $parent_jobid
-    
+    echo NAME      : $jobname
+    echo NCPUS     : $ncpus
+    echo OVERWRITE : $overwrite_array
+    echo ARRAYS    : $arrays
+    echo PJOBID    : $parent_jobid
+
     # Create log folder
     FLOG=$FROOT/logs/$jobname
     mkdir -p $FLOG
     echo .. created log folder logs/$jobname
 
     # Run job
-    if [ $parent_jobid -ne "X" ]
+    if [ $parent_jobid = "X" ]
     then
         echo .. submitting job with array $arrays and no dependency
         jobid=$(sbatch -J $jobname --cpus-per-task=$ncpus \
-            --array=$arrays --parsable --export=ALL $jobscript)
+            --array=$arrays --parsable --export=ALL $JOBSCRIPT)
     else    
         echo .. submitting job with array $arrays and dependency on $parent_jobid
         jobid=$(sbatch -J $jobname --cpus-per-task=$ncpus \
-            --array=$arrays --dependency=afterany:${parent_jobid} --export=ALL $jobscript)
+            --array=$arrays --dependency=afterany:${parent_jobid} --export=ALL $JOBSCRIPT)
     fi    
 
     # Iterate jobid
-    JOBCONFIGS[$ijob,5]=$jobid
+    JOBCONFIGS["$ijob,5"]=$jobid
 
     echo .. All done. Submitted job $jobid and moving to next job.
-    echo ----------------------------------
+    echo ----------------------------------------------------------
     echo
 
 done    
