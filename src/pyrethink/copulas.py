fix: fixed survival computation

src/pyrethink/copulas.py
@@ -1,5 +1,4 @@
 import math
-from itertools import combinations
 
 import numpy as np
 
@@ -234,25 +233,30 @@ class Copula():
 
         return z
 
-    def get_rv(self, iselected):
+    def get_iselect(self, iselect=None):
+        nsta = self.nstations
+        return np.arange(nsta) if iselect is None \
+            else iselect
+
+    def get_rv(self, iselect=None):
+        iselect = self.get_iselect(iselect)
         copula_type = self.copula_type
-        scorr = self.corr_rescaled[iselected][:, iselected]
+        scorr = self.corr_rescaled[iselect][:, iselect]
         scorr = np.ascontiguousarray(scorr)
         mu = np.zeros(self.nstations)
 
         if copula_type == 1:
             df = self.copula_shape
-            return mvt(loc=mu[iselected], shape=scorr, df=df)
+            return mvt(loc=mu[iselect], shape=scorr, df=df)
         elif copula_type == 0:
-            return mvn(mean=mu[iselected], cov=scorr)
+            return mvn(mean=mu[iselect], cov=scorr)
         else:
             errmsg = "Cannot get rv for copula type {copula_type}."
             raise ValueError(errmsg)
 
     def get_sets_iterator(self, ipart, iselect):
+        iselect = self.get_iselect(iselect)
         nsta = self.nstations
-        iselect = np.arange(nsta) if iselect is None \
-            else iselect
         isin = np.zeros(nsta).astype(bool)
         isin[iselect] = True
 
@@ -289,27 +293,17 @@ class Copula():
 
         return cdf
 
-    def survival_given_partition(self, ipart, z):
+    def survival_given_partition(self, ipart, z, iselect=None):
+        iselect = self.get_iselect(iselect)
         nsta = self.nstations
         if len(z) != nsta:
             errmsg = f"Expected z of length {nsta}."
             raise ValueError(errmsg)
 
-        surv = 1. - norm.cdf(z).sum()
-        sets = self.partitions.ipart2sets(ipart)
-
-        for nv in range(2, nsta + 1):
-            f = (-1)**nv
-            for ix in combinations(np.arange(nsta), nv):
-                # Compute cdf for each combination
-                ix = np.array(ix)
-                cdf = 1.
-                for s in np.unique(sets[ix]):
-                    ixs = ix[sets[ix] == s]
-                    rvs = self.get_rv(ixs)
-                    cdf *= rvs.cdf(z[ixs])
-
-                surv += f * cdf
+        surv = 1.
+        for idx in self.get_sets_iterator(ipart, iselect):
+            rv = self.get_rv(idx)
+            surv *= rv.cdf(-z[idx])
 
         return surv
 

src/pyrethink/postpredchecks.py
@@ -7,9 +7,9 @@ from scipy.stats import kendalltau
 from floodstan.marginals import lh_moments
 from floodstan.marginals import GEV
 
-from pyrethink.copula import Copula
+from pyrethink.copulas import Copula
 
-PERC_TAILS_DEFAULT = np.arange(50, 100, 2)
+PERC_TAILS_DEFAULT = np.arange(50, 100, 5)
 
 
 def univariate_statistics(data, eta=2, perc_tails=np.arange(50, 110, 10)):
@@ -73,6 +73,12 @@ def multivariate_dependence_statistics(data,
     return df
 
 
+def dependence2series(dep):
+    se = pd.melt(dep.reset_index(), id_vars="percentile")
+    se.loc[:, "stat"] = se.variable + "_q" + se.percentile.astype(str)
+    return se.set_index("stat").loc[:, "value"]
+
+
 def bivariate_dependence_statistics(data,
                                     perc_tails=PERC_TAILS_DEFAULT):
     data = np.array(data)
@@ -107,7 +113,6 @@ def compute_predictive_checks(metric_obs, metric_sim):
     sim_std = metric_sim.std()
 
     pvalue = (metric_sim - metric_obs >= 0).sum(axis=0) / (nparams + 1)
-
     diff = (metric_sim < metric_obs) | (metric_sim > metric_obs)
     ndiff = diff.sum()
     pvalue_discr = (metric_sim - metric_obs > 0).sum(axis=0) / (ndiff + 1)
@@ -149,12 +154,16 @@ def posterior_predictive_checks(yobs, params,
     biv_obs = []
     for i1, i2 in combs(range(nsta), 2):
         m = bivariate_dependence_statistics(yobs[:, [i1, i2]])
-        m.name = f"bivariate[{i1 + 1},{i2 + 1}]"
-        biv_obs.append(m)
+        se = dependence2series(m["dependence"])
+        for cn in ["kendalltau", "kendalltau_high"]:
+            se[cn] = m[cn]
+        se.name = f"bivariate[{i1 + 1},{i2 + 1}]"
+        biv_obs.append(se)
 
     biv_obs = pd.DataFrame(biv_obs).T
 
     multi_obs = multivariate_dependence_statistics(yobs)
+    multi_obs = dependence2series(multi_obs)
 
     # Marginal
     gev = GEV()
@@ -187,9 +196,14 @@ def posterior_predictive_checks(yobs, params,
 
         for i1, i2 in combs(range(nsta), 2):
             bi = bivariate_dependence_statistics(ysim[:, [i1, i2]])
-            biv_sim[(i1, i2)].append(bi)
+            se = dependence2series(bi["dependence"])
+            for cn in ["kendalltau", "kendalltau_high"]:
+                se[cn] = bi[cn]
+            se.name = f"bivariate[{i1 + 1},{i2 + 1}]"
+            biv_sim[(i1, i2)].append(se)
 
         mult = multivariate_dependence_statistics(ysim)
+        mult = dependence2series(mult)
         multi_sim.append(pd.DataFrame(mult).T)
 
     # Compute predictiive checks

tests/test_copulas.py
@@ -35,7 +35,7 @@ FTESTS = Path(__file__).resolve().parent
 def get_type(copula_shape):
     return 0 if abs(copula_shape) < 1e-10 else 1
 
-@pytest.mark.parametrize("copula_shape", [0., 2.5, 5., 10.])
+@pytest.mark.parametrize("copula_shape", [0., 3., 6., 10.])
 def test_copula_marginals(copula_shape, allclose):
     copula_type = get_type(copula_shape)
 
@@ -220,7 +220,7 @@ def test_copula_sample(copula_shape, allclose):
     assert allclose(pp, expected, atol=5e-3)
 
 
-@pytest.mark.parametrize("copula_shape", [0., 5., 10.])
+@pytest.mark.parametrize("copula_shape", [0., 3., 5., 7., 10.])
 @pytest.mark.parametrize("repeat", range(10))
 def test_copula_cdf(repeat, copula_shape, allclose):
     copula_type = get_type(copula_shape)
@@ -233,25 +233,17 @@ def test_copula_cdf(repeat, copula_shape, allclose):
     nsamples = 500000
 
     z0 = np.random.uniform(-0.5, 0.5, size=nsta)
-    z0 = np.array([0.49, -0.5, 0.39, -0.1])
-    print()
-    print()
-    print(f"z0 = {z0.round(2)}")
-    print()
-
-    atol = 2e-2
+    rtol = 2e-2
 
     for ipart in range(ccs.partitions.nsubsets):
         z = ccs.sample_z_given_partition(ipart, nsamples)
 
         ce = (z - z0[None, :] < 0).all(axis=1).sum() / nsamples
         c0 = ccs.cdf_given_partition(ipart, z0)
-        assert allclose(ce, c0, atol=atol)
+        rerr = abs(math.log(ce) - math.log(c0))
+        assert rerr < rtol
 
         se = (z - z0[None, :] >= 0).all(axis=1).sum() / nsamples
         s0 = ccs.survival_given_partition(ipart, z0)
-        passed = abs(se - s0) < atol
-
-        print(f"ipart={ipart} / s0={s0:0.3f} se={se:0.3f} passed={passed}")
-        #assert allclose(se, s0, atol=atol)
-
+        rerr = abs(math.log(se) - math.log(s0))
+        assert rerr < rtol